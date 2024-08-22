a45d5950f43cea79e06b4b2b506222445a5abcaa
Amirali <106759325+AAFBuniversityGit@users.noreply.github.com>
2024-08-22 23:13:40 +0330

    Created GitHubIssueData.cs Class

--- GitHubIssueClassification/Program.cs
+++ GitHubIssueClassification/Program.cs
@@ -1,2 +1,13 @@
 ﻿using Microsoft.ML;
 using GitHubIssueClassification;
+
+
+var _appPath = Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]) ?? ".";
+var _trainDataPath = Path.Combine(_appPath, "..", "..", "..", "Data", "issues_train.tsv");
+var _testDataPath = Path.Combine(_appPath, "..", "..", "..", "Data", "issues_test.tsv");
+var _modelPath = Path.Combine(_appPath, "..", "..", "..", "Models", "model.zip");
+
+MLContext _mlContext;
+PredictionEngine<GitHubIssue, IssuePrediction> _predEngine;
+ITransformer _trainedModel;
+IDataView _trainingDataView;
